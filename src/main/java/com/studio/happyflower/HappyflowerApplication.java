13204b722dc7fe53106708fef3b95cb04357edeb
Jakub Cholewa <tom7177@gmail.com>
2019-04-22 22:50:26 +0200

    git init

---
+++ src/main/java/com/studio/happyflower/HappyflowerApplication.java
@@ -0,0 +1,13 @@
+package com.studio.happyflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyflowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyflowerApplication.class, args);
+    }
+
+}
