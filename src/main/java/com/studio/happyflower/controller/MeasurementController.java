a64c85040c3cb81577197be3dd62514868eaa54c
Jakub Cholewa <tom7177@gmail.com>
2019-05-20 20:36:45 +0200

    CRUD for Measurements

--- src/main/java/com/studio/happyflower/controller/MeasurementController.java
+++ src/main/java/com/studio/happyflower/controller/MeasurementController.java
@@ -1,14 +1,20 @@
 package com.studio.happyflower.controller;
 
+import com.studio.happyflower.model.entity.Measurement;
+import com.studio.happyflower.model.entity.User;
 import com.studio.happyflower.model.repository.MeasurementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/meausrement")
+@RequestMapping("/api/measurements")
 @CrossOrigin
 public class MeasurementController
 {
@@ -18,5 +24,48 @@ public class MeasurementController
     @GetMapping
     public Iterable findAll() {
         return measurementRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<?> findById(@PathVariable Long id) {
+        Optional<Measurement> measurement = measurementRepository.findById(id);
+        if(measurement.isPresent()) {
+            return ResponseEntity.ok().body(measurement);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Measurement> createMeasurement(@Valid @RequestBody Measurement measurement) throws URISyntaxException {
+        Measurement result = measurementRepository.save(measurement);
+        return ResponseEntity.created(new URI("/api/measurements/" + result.getId()))
+                .body(result);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Measurement> updateMeasurement(@PathVariable Long id, @Valid @RequestBody Measurement updatedMeasurement) {
+        Optional<Measurement> measurementOptional = measurementRepository.findById(id);
+        if(measurementOptional.isPresent()){
+            Measurement measurement = measurementOptional.get();
+            measurement.setPlant(updatedMeasurement.getPlant());
+            measurement.setDateTime(updatedMeasurement.getDateTime());
+            measurement.setHumidity(updatedMeasurement.getHumidity());
+            measurement.setSoilMosture(updatedMeasurement.getSoilMosture());
+            measurement.setTemperature(updatedMeasurement.getTemperature());
+            measurementRepository.save(measurement);
+            return ResponseEntity.ok().body(measurement);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> deleteMeasurement(@PathVariable Long id) {
+        measurementRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/studio/happyflower/model/entity/Measurement.java
+++ src/main/java/com/studio/happyflower/model/entity/Measurement.java
@@ -85,7 +85,6 @@ public class Measurement {
     }
 
 
-
     public Measurement() {
     }
 }
